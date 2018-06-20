fbe22bbce2591a82469514161fa0255ace666c5d
eversonott <everson.o@hotmai.com>
2018-06-20 16:27:54 -0300

    adicao de comentarios

--- aula01.cpp
+++ aula01.cpp
@@ -2,12 +2,19 @@
 #include <stdio.h>
 
 int main(int arg, char*argv[]){
+    //declara uma variavel do tipo float
     float numero = 34.78;
 
+    //declaro um ponteiro de float
     float *pf;
+    //aponta o ponteiro para o lugar onde esta a variavel numero usando (&)
     pf = &numero;
+    //imprime 1
+    printf("%f\n",numero);
     printf("%f\n",*pf);
     *pf = 1.5;
+    //imprime 2
+    printf("%f\n",numero);
     printf("%f\n",*pf);
     return 0;
 }
